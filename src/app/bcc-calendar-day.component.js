3959c0b7aeea46c1e03f384be068dd4eb5987216
RJ Russell <RJ-Russell@users.noreply.github.com>
2017-09-01 19:46:25 -0600

    add week and day components

---
+++ src/app/bcc-calendar-day.component.js
@@ -0,0 +1,9 @@
+const bccCalendarDay = {
+  bindings: {},
+  controller: /*@ngInject*/ function () {
+    let ctrl = this;
+  },
+  template: ''
+};
+
+angular.module('bcc-calendar').component('bccCalendarDay', bccCalendarDay);
---
+++ src/app/bcc-calendar-week.component.js
@@ -0,0 +1,9 @@
+const bccCalendarWeek = {
+  bindings: {},
+  controller: /*@ngInject*/ function () {
+    let ctrl = this;
+  },
+  template: ''
+};
+
+angular.module('bcc-calendar').component('bccCalendarWeek', bccCalendarWeek);
